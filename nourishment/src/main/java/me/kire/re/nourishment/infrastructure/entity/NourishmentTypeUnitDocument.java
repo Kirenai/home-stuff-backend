523b9223f70a4aef8bfe019ef3250f892efc4bdb
Kirenai <pool_dota2@hotmail.com>
2025-06-05 01:52:57 -0500

    feat(nourishment): declare NourishmentTypeUnitDocument as final

--- nourishment/src/main/java/me/kire/re/nourishment/infrastructure/entity/NourishmentTypeUnitDocument.java
+++ nourishment/src/main/java/me/kire/re/nourishment/infrastructure/entity/NourishmentTypeUnitDocument.java
@@ -9,6 +9,6 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class NourishmentTypeUnitDocument extends NourishmentTypeDocument {
+public final class NourishmentTypeUnitDocument extends NourishmentTypeDocument {
     private Integer unit;
 }
